765df57b906d089af24b35eaa1ac97711476a8b2
Sebastian Moder <moder.sebastian@gmail.com>
2024-11-06 16:07:06 +0100

    first content

---
+++ pages/_meta.js
@@ -0,0 +1,12 @@
+export default {
+  "index": "Introduction",
+  "genai": "Understanding Generative AI",
+  "prompting": "Prompting AI",
+  "about": "About",
+  // contact: {
+  //   title: "Contact ↗",
+  //   type: "page",
+  //   href: "https://twitter.com/shuding_",
+  //   newWindow: true
+  // }
+}
