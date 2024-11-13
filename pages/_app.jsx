3b760ae1cf50954223a61e2d256ec12eb9d2ed65
Sebastian Moder <moder.sebastian@gmail.com>
2024-11-13 18:33:28 +0100

    improved content

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,8 @@
 import '../styles/global.css'
 import '../styles/chat.css'
+import { Callout } from 'nextra/components'
+
+
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
--- pages/prompting/_meta.js
+++ pages/prompting/_meta.js
@@ -3,6 +3,6 @@ export default {
   instruct: 'Instruct the model throughout the prompt',
   use_delimiters: 'Use delimiters and special tokens',
   time_to_think: 'Give the model time to think',
-  give_examples: 'Give examples',
+  give_examples: 'Provide examples to guide the output',
   further_resources: 'Further resources',
 }
