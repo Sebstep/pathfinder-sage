02407bdfa0b67b4a24a7715613c84217434d3e7f
Sebastian Moder <moder.sebastian@gmail.com>
2024-11-14 12:27:17 +0100

    removed unused callout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,5 @@
 import '../styles/global.css'
 import '../styles/chat.css'
-import { Callout } from 'nextra/components'
-
-
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
