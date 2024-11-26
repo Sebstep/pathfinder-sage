2fa2e210a25de18c5e752bbe90879b01ada0f7b4
Sebastian Moder <moder.sebastian@gmail.com>
2024-11-26 14:10:44 +0100

    Refactor footer and update Tailwind config

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Link from "next/link";
+import Image from 'next/image';
 
 function Logo() {
     return (
-        <div className="w-full text-lg font-extrabold md:w-fit md:text-left">
+        <div className="text-lg font-extrabold">
             <span className="text-eu-blue">Erasmus+ Pathfinder</span>
         </div>
-    )
+    );
 }
+
 
 export default function MultiColumns() {
     return (
-        <footer className="mx-auto flex flex-col items-start space-y-12 px-8 md:flex-row md:space-y-0 md:space-x-12 md:px-12 md:justify-between">
-            {/* First Column */}
-            <div className="flex w-full flex-col space-y-4 text-center md:w-5/12 md:text-left">
+        <footer className="container mx-auto px-8 py-12 space-y-12 md:flex md:space-y-0 md:justify-between">
+            {/* left Column */}
+            <div className="flex flex-col space-y-4 md:w-5/12">
                 <Logo />
                 <p className="text-sm text-slate-600">
                     Pioneering AI Technology in Higher Education to Facilitate
@@ -22,18 +24,21 @@ export default function MultiColumns() {
                 </p>
                 <p className="text-sm text-slate-600">
                     Project:{' '}
-                    <a href="https://erasmus-plus.ec.europa.eu/projects/search/details/2023-2-LI01-KA220-HED-000178579">
-                        2023-2-LI01-KA220-HED-000178579{' '}
-                        <i className="fa-solid fa-arrow-up-right-from-square"></i>
-                    </a>
+                    <Link
+                        href="https://erasmus-plus.ec.europa.eu/projects/search/details/2023-2-LI01-KA220-HED-000178579"
+                        className="text-blue-500 hover:underline"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        2023-2-LI01-KA220-HED-000178579
+                    </Link>
                 </p>
                 <p className="text-sm text-slate-600">
                     &copy; {new Date().getFullYear()} Pathfinder Project. All
                     rights reserved.
                 </p>
             </div>
-            {/* Fourth Column */}
-            <div className="flex w-full flex-col space-y-4 text-center md:w-5/12 md:text-left">
+            {/* Right Column */}
+            <div className="flex flex-col space-y-4 md:w-5/12">
                 <div className="text-sm font-semibold">Declarations</div>
                 <p className="text-sm text-slate-600">
                     Funded by the European Union. Views and opinions expressed
@@ -43,13 +48,15 @@ export default function MultiColumns() {
                     Neither the European Union nor EACEA can be held responsible
                     for them.
                 </p>
-                <Image
+                <div className="mt-4">
+                    <Image
                     src="/funded-by-eu-wide.png"
                     alt=""
                     width={200}
-                    height={200}
-                    className="w-full md:w-1/2"
-                />
+                    height={50}
+                    className="mx-auto md:mx-0"
+                    />
+                </div>
             </div>
         </footer>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './app/**/*.{js,ts,jsx,tsx,md,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'eu-blue': '#004494',
+                'eu-yellow': '#ffd617',
+                'eu-grey': '#404040',
+            },
+        },
+    },
+    plugins: [],
 }
-
