3ef07e6306f50d0c2ac97736da7421b8992473dc
Sebastian Moder <moder.sebastian@gmail.com>
2024-12-01 23:07:05 +0100

    fix sitemap base url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://ai-pathfinder.eu',
+    siteUrl: process.env.SITE_URL || 'https://sage.ai-pathfinder.eu',
     generateRobotsTxt: true, // (optional)
     generateIndexSitemap: false, // (optional)
     outputDir: 'public', // (optional)
