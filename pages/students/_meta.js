4acda33b31ecf9b72bde03a28e7225adde7438f3
Sebastian Moder <moder.sebastian@gmail.com>
2025-03-04 15:25:51 +0100

    Improved upon assignments and exams, moved page

--- pages/students/_meta.js
+++ pages/students/_meta.js
@@ -1,5 +1,4 @@
 export default {
-    ai_detection: 'Why AI detection does not work',
     exams: 'Exams & how to assess learning',
     tutor: 'Using AI as a tutor',
 }
--- pages/teaching/_meta.js
+++ pages/teaching/_meta.js
@@ -1,6 +1,7 @@
 export default {
-    lesson_planning: 'Planning interactive lessons',
+    ai_detection: 'Why AI detection does not work',
     assignments: 'Creating tasks & assignments',
+    lesson_planning: 'Planning interactive lessons',
     feedback: 'Providing personalized feedback',
     admin_work: 'Automating administrative work',
 }
