8186a93b26edeaa88acd3abba6cb02d05c0acb13
Sebastian Moder <moder.sebastian@gmail.com>
2025-05-04 15:51:43 +0200

    Setup new pages

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,7 +1,19 @@
 export default {
     index: 'Introduction',
     genai: 'Basics of Generative AI',
-    prompting: 'Basic Prompt Engineering',
+    prompting: 'Basics of Prompt Engineering',
     teaching: "Teachers' Tasks",
     students: "Students' Experiences",
+    casereports: {
+        title: 'Case Reports & Experiences',
+        display: 'hidden',
+    },
+    teachertraining: {
+        title: 'Training Teachers on AI Use',
+        display: 'hidden',
+    },
+    aitools: {
+        title: 'Other AI tools',
+        display: 'hidden',
+    },
 }
